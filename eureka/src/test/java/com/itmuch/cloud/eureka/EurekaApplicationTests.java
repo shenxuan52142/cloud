6520f5cabd5087e5465b6c62a7f4a5e0594d49b4
shenxuan52142 <shenxuan1hao@163.com>
2020-06-10 15:05:51 +0800

    创建为服务

---
+++ eureka/src/test/java/com/itmuch/cloud/eureka/EurekaApplicationTests.java
@@ -0,0 +1,9 @@
+package com.itmuch.cloud.eureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+
+}
--- shenxuan-2/src/main/java/com/itmuch/cloud/mircroservicesimpleprovideruser/MircroserviceSimpleProviderUserApplication.java
+++ shenxuan-2/src/main/java/com/itmuch/cloud/mircroservicesimpleprovideruser/MircroserviceSimpleProviderUserApplication.java
@@ -3,12 +3,10 @@ package com.itmuch.cloud.mircroservicesimpleprovideruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class MircroserviceSimpleProviderUserApplication {
 	@Bean
 	public RestTemplate restTemplate(){
