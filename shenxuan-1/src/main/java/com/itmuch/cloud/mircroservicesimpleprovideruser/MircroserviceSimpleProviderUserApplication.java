b30adc33d3c7edf9bb498746b167107f4e31ccf9
shenxuan52142 <shenxuan1hao@163.com>
2020-06-11 15:34:27 +0800

    6-11

--- shenxuan-1/src/main/java/com/itmuch/cloud/mircroservicesimpleprovideruser/MircroserviceSimpleProviderUserApplication.java
+++ shenxuan-1/src/main/java/com/itmuch/cloud/mircroservicesimpleprovideruser/MircroserviceSimpleProviderUserApplication.java
@@ -3,21 +3,13 @@ package com.itmuch.cloud.mircroservicesimpleprovideruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableFeignClients
 public class MircroserviceSimpleProviderUserApplication {
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(MircroserviceSimpleProviderUserApplication.class, args);
 	}
--- shenxuan-1/src/main/java/com/itmuch/cloud/mircroservicesimpleprovideruser/controller/UserController.java
+++ shenxuan-1/src/main/java/com/itmuch/cloud/mircroservicesimpleprovideruser/controller/UserController.java
@@ -1,26 +1,21 @@
 package com.itmuch.cloud.mircroservicesimpleprovideruser.controller;
 
-import com.itmuch.cloud.mircroservicesimpleprovideruser.jpa.UserRepository;
-import com.itmuch.cloud.mircroservicesimpleprovideruser.vo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-
+@RefreshScope
 @RestController
 public class UserController {
     public static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @GetMapping("/{id}")
+    @Value("${profile}")
+    private String name;
+    @GetMapping("/{getName}")
     public String findById(@PathVariable Long id){
-        return "ssss";
+        return name;
     }
 }
